0e2cc7897fa4b43027a74c58a77a7d0d0096234b
Andrés Aranguren <31296034+afarangurens@users.noreply.github.com>
2024-06-10 11:44:12 -0500

    Agregado el cálculo de ventas promedio en precios en la ruta de cantidad y ventas totales por llave (KeyEmployee, KeyProduct, KeyStore)

--- app/services/parquet_service.py
+++ app/services/parquet_service.py
@@ -71,25 +71,70 @@ class ParquetService:
                 f"File {filename} not found in {self.data_dir}"
             )
 
+        loaded_df = self._load_filtered_dataframe(file_path, key_type)
+        
+        start_date = self._date_to_str(start_date)
+        end_date = self._date_to_str(end_date)
+
+        filtered_df = self._filter_df(loaded_df, start_date, end_date, key_type, key_value)
+
+        return filtered_df
+    
+
+    def _load_filtered_dataframe(self, file_path, key_type) -> pd.DataFrame:
+        """
+        Load a filtered DataFrame from a Parquet file based on a specified key type.
+
+        :param file_path: Path to the Parquet file.
+        :param key_type: Type of key to filter by (KeyEmployee, KeyProduct, or KeyStore).
+        :return: Filtered DataFrame.
+        """
         try:
             df = pd.read_parquet(file_path, columns=['KeyDate', key_type, 'Qty', 'Amount'])
         except ValueError:
             raise HTTPException(status_code=400, detail=f"{key_type} not found in the Parquet files.")
-        
-        start_date = pd.to_datetime(start_date, format='%Y-%m-%d')
-        end_date = pd.to_datetime(end_date, format='%Y-%m-%d')
+
+        return df
+    
+    def _date_to_str(self, date) -> pd.Timestamp:
+        """
+        Convert a date string to a pandas Timestamp object.
+
+        :param date: Date string in the format 'YYYY-MM-DD'.
+        :return: Pandas Timestamp object.
+        """
+        return pd.to_datetime(date, format='%Y-%m-%d')
+    
+    def _filter_df(self, df: pd.DataFrame, start_date: pd.Timestamp, end_date: pd.Timestamp, key_type: str, key_value: str = None) -> list[dict]:
+        """
+        Filter DataFrame based on date range and optionally by key value, then aggregate the results.
+
+        :param df: DataFrame to filter.
+        :param start_date: Start date for filtering.
+        :param end_date: End date for filtering.
+        :param key_type: Type of key to filter by (KeyEmployee, KeyProduct, or KeyStore).
+        :param key_value: Value of the key to filter by.
+        :return: Aggregated result as a list of dictionaries.
+        """
+
+        if df is None or df.empty:
+            raise HTTPException(status_code=400, detail="DataFrame is empty or None.")
+
+        if start_date > end_date:
+            raise HTTPException(status_code=400, detail="Start date cannot be after end date.")
+
+        if key_type not in df.columns:
+            raise HTTPException(status_code=400, detail=f"{key_type} not found in DataFrame columns.")
 
         df['KeyDate'] = pd.to_datetime(df['KeyDate'], format='%m/%d/%Y %I:%M %p')
+
         df_filtered = df[(df['KeyDate'] >= start_date) & (df['KeyDate'] <= end_date)]
 
         if key_value:
             df_filtered = df_filtered[df_filtered[key_type] == key_value]
-            df_grouped = df_filtered.groupby(key_type).agg({'Qty': 'sum', 'Amount': 'sum'}).reset_index()
-        else:
-            df_grouped = df_filtered.groupby(key_type).agg({'Qty': 'sum', 'Amount': 'sum'}).reset_index()
 
+        df_filtered['AvgAmount'] = df_filtered['Amount'] / df_filtered['Qty']
+
+        df_grouped = df_filtered.groupby(key_type).agg({'Qty': 'sum', 'Amount': 'sum', 'AvgAmount': 'mean'}).reset_index()
 
         return df_grouped.to_dict(orient="records")
-    
-
-        
